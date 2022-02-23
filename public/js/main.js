c1b595abfa21f4168594252f18d318f0f3065cba
bilal001 <56475491+Ambush64@users.noreply.github.com>
2022-02-23 21:25:04 +0530

    https fetch

--- public/js/main.js
+++ public/js/main.js
@@ -14,7 +14,7 @@ const getInfo = async (e) => {
     dataHide.classList.add("data_hide");
   } else {
     try {
-      let url = `http://api.openweathermap.org/data/2.5/weather?q=${cityVal}&appid=bd17567668855041c9ccca90539f2aa5`;
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityVal}&appid=bd17567668855041c9ccca90539f2aa5`;
       const response = await fetch(url);
       const data = await response.json();
       const arrData = [data];
